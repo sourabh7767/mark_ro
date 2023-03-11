b77b44a35e96dc2c45c772bfd15e34e287645b64
arun.arora.escalon <arun.arora@escalon.services>
2023-03-11 22:22:11 +0530

    project name changes

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-        <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-        <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+        <meta name="description" content="Valley Wide Collision admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+        <meta name="keywords" content="admin template, Valley Wide Collision admin template, dashboard template, flat admin template, responsive admin template, web app">
         <meta name="author" content="Devendra Rajput">
         <title> @yield('title') </title>
         <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -88,7 +88,7 @@
                                         </g>
                                     </g>
                                 </svg>
-                                <h2 class="brand-text text-primary ms-1">Vuexy</h2>
+                                <h2 class="brand-text text-primary ms-1">Valley Wide Collision</h2>
                             </a>
                             <!-- /Brand logo-->
                             <!-- Left Text-->
