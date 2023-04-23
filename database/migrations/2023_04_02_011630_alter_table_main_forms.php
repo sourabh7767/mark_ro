f09dd1d7a8c257f9d5530853e314ef91dd669716
sourabh7767 <saurav7767@gmail.com>
2023-04-23 22:14:40 +0530

    updates

--- database/migrations/2023_04_02_011630_alter_table_main_forms.php
+++ database/migrations/2023_04_02_011630_alter_table_main_forms.php
@@ -14,7 +14,25 @@ class AlterTableMainForms extends Migration
     public function up()
     {
         Schema::table('main_forms', function (Blueprint $table) {
-            $table->string('status')->nullable()->default("open");
+            $table->string('status')->nullable();
+            $table->date('date_in')->nullable();
+            $table->date('date_out')->nullable();
+            $table->date('target_date')->nullable();
+            $table->date('due_date')->nullable();
+            $table->float('labour_hours', 8, 2)->nullable();
+            $table->float('sales_amount', 8, 2)->nullable();
+
+
+            $table->integer('wheel')->comment("0=>not checked,1=>checked")->default(0)->nullable();
+            $table->date('wheel_date')->nullable();
+            $table->integer('alignment')->comment("0=>not checked,1=>checked")->default(0)->nullable();
+            $table->date('alignment_date')->nullable();
+            $table->integer('decals')->comment("0=>not checked,1=>checked")->default(0)->nullable();
+            $table->date('decals_date')->nullable();
+            $table->integer('glass')->comment("0=>not checked,1=>checked")->default(0)->nullable();
+            $table->date('glass_date')->nullable();
+            $table->integer('adas')->comment("0=>not checked,1=>checked")->default(0)->nullable();
+            $table->date('adas_date')->nullable();
         });
     }
 
